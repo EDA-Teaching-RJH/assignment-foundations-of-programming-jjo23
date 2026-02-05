0746ac15a1bfcbdc548aa8a5986110a831e571d7
jjo23 <jjo23@kent.ac.uk>
2026-02-05 17:40:15 +0000

    added elements to their respectives lists

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,4 +1,6 @@
-n = []
-r = []
-d = []
-id = []
+# note i havent actually watched star trek so i dont have a clue if these characters and their divisions are factual
+
+n = ["Airiam", "Jonathan Archer", "Ascencia", "Soji Asha", "Ayala"]
+r = ["Lt, Commander", "Captain", "Civilian", "Civilian", "Lieutenant"]
+d = ["Command", "Operations", "None", "None", "Security"]
+id = [5423, 9867, 9068, 1654, 3459]
